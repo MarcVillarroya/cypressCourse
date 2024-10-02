f7e6e05b3ec3887c6598af2b482b9e1c0ae7bffd
MarcVillarroya <marcvillarroyafernandez@gmail.com>
2024-10-02 16:38:49 +0200

    report config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Allure Test', () => {
+  it('should pass and generate results', () => {
+    cy.visit('https://example.cypress.io');
+    cy.contains('type').click();
+    cy.url().should('include', '/commands/actions');
+    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com');
+  });
+});
