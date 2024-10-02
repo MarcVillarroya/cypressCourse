649eebdbba871c5c92f2c30b101d325caa5239e1
MarcVillarroya <marcvillarroyafernandez@gmail.com>
2024-10-02 17:08:43 +0200

    loginPage

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,10 @@
-describe('Allure Test', () => {
-  it('should pass and generate results', () => {
-    cy.visit('https://example.cypress.io');
-    cy.contains('type').click();
-    cy.url().should('include', '/commands/actions');
-    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com');
+const { LoginMethods } = require("./pages/login/login.methods");
+
+describe('Invalid login', () => {
+  it('should fail login given incorrect credentials', () => {
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login('username', 'password')
+    cy.wait(5000)
   });
 });
